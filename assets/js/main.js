7c5a06741526968741f9be8c56ba248a500e6703
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-09 18:55:14 +0100

    testingPhase

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+console.log("test");
+
+/* 
+
+Mathi.PI
+
+*/
+
+const body = document.querySelector("body");
+
+console.log(Math.PI);
+
+const PI = Math.PI;
+console.log(PI);
+
+roundedPI = PI.toFixed(2)
+console.log(roundedPI);
+document.write(roundedPI + "<br>");
+
+body.innerHTML += roundedPI
